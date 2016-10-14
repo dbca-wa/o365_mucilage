7367012386a99b7bf9f46979221da4c1f2a1a8c4
Ashley Felton <ashley@ropable.com>
2016-10-14 08:21:18 +0800

    Added script to output O365 licenced users to CSV.

---
+++ Get-365LicensedUsers.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module ActiveDirectory;
+Import-Module -Force 'C:\cron\creds.psm1';
+
+$users = Get-MsolUser -All | Where {$_.isLicensed -eq "True" -and ("DPaW:ENTERPRISEPACK" -in $_.licenses.accountSkuId)}
+$user=$users | Select UserPrincipalName
+Del C:\cron\o365_licensed.csv
+$user | Export-Csv C:\cron\o365_licensed.csv
