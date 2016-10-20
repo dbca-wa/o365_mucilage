0e80cc793e5b590b4f2d6e76ef9c90350675372d
Scott Percival <scott.percival@dpaw.wa.gov.au>
2016-10-20 14:54:44 +0800

    Better comments.

--- directory_wrangler.ps1
+++ directory_wrangler.ps1
@@ -11,13 +11,14 @@ try {
     # Store the domain max password age in days.
     $DefaultmaxPasswordAgeDays = (Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge.Days;
 
-    # get all the mailbox records (local Mailboxes and Office 365 RemoteMailboxes)
+    # Get all the mailbox records (local Mailboxes and Office 365 RemoteMailboxes)
     $mailboxes = $(Get-Mailbox -ResultSize unlimited | select userprincipalname, primarysmtpaddress, recipienttypedetails) + $(Get-RemoteMailbox -ResultSize unlimited | select userprincipalname, primarysmtpaddress, recipienttypedetails);
     $mailboxes | convertto-json > 'C:\cron\mailboxes.json';
     
-    # read the full user DB from OIM CMS. 
+    # Read the full user DB from OIM CMS (all DepartmentUser objects) via the OIM CMS API.
+    # NOTE: $user_api is set in C:\cron\creds.psm1
     $users = Invoke-RestMethod ("{0}?all" -f $user_api) -WebSession $oimsession;
-    # do a workaround to vault PowerShell's dumb 10mb JSON limit
+    # Do a workaround to vault PowerShell's dumb 10mb JSON limit.
     if (-not $users.objects) {
         [void][System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions");
         $json = New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer;
@@ -25,12 +26,12 @@ try {
         $users = $json.Deserialize($users, [System.Object]);
     }
     
-    # user object attributes we care about
+    # Define user object attributes that we care about.
     $keynames = @("Title", "DisplayName", "GivenName", "Surname", "Company", "physicalDeliveryOfficeName", "StreetAddress", "Division", "Department", "Country", "State",
         "wWWHomePage", "Manager", "EmployeeID", "EmployeeNumber", "HomePhone", "telephoneNumber", "Mobile", "Fax", "employeeType");
     $adprops = $keynames + @("EmailAddress", "UserPrincipalName", "Modified", "AccountExpirationDate", "Info", "pwdLastSet");
     
-    # read the user list from AD. apply a rough filter for accounts we want to load into OIM CMS:
+    # Read the user list from AD. Apply a rough filter for accounts we want to load into OIM CMS:
     # - email address is *.wa.gov.au or dpaw.onmicrosoft.com
     # - has both a first name and surname
     # - DN contains a sub-OU called "Users"
@@ -39,7 +40,7 @@ try {
     $adusers += Get-ADUser -server $adserver -Filter {EmailAddress -like "*@dpaw.onmicrosoft.com"} -Properties $adprops;
     Log $("Processing {0} users" -f $adusers.Length);
 
-    # If an AD user doesn't exist in OIM CMS, load the data from current AD record in via the REST API
+    # If an AD user doesn't exist in the OIM CMS, load the data from current AD record via the REST API.
     ForEach ($aduser in $adusers | where { $_.EmailAddress -notin $users.objects.email }) {
         $simpleuser = $aduser | select ObjectGUID, DistinguishedName, Name, Title, SamAccountName, GivenName, Surname, EmailAddress, Modified, Enabled, AccountExpirationDate, pwdLastSet, employeeType;
         $simpleuser.Modified = Get-Date $aduser.Modified -Format s;
@@ -48,17 +49,18 @@ try {
         }
         $simpleuser | Add-Member -type NoteProperty -name PasswordMaxAgeDays -value $DefaultmaxPasswordAgeDays;
         $userjson = $simpleuser | ConvertTo-Json;
+        # Here we POST to the API endpoint to create a new DepartmentUser in the CMS.
         (Invoke-RestMethod $user_api -Body $userjson -Method Post -ContentType "application/json" -Verbose -WebSession $oimsession).ad_data;
     }
 
-    # for each OIM CMS DepartmentUser
+    # For each OIM CMS DepartmentUser...
     foreach ($user in $users.objects) {
-        # find the equivalent Active Directory Object
+        # ...find the equivalent Active Directory Object.
         $aduser = $adusers | where EmailAddress -like $($user.email);
         If ($aduser) {
-            # if the OIM CMS user object was modified in the last hour
+            # If the OIM CMS user object was modified in the last hour...
             if (($(Get-Date) - (New-TimeSpan -Minutes 60)) -lt $(Get-Date $user.date_updated) -and ($aduser.Modified -lt $(Get-Date $user.date_updated))) {
-                # set all the properties on the AD object to match the OIM CMS object
+                # ...set all the properties on the AD object to match the OIM CMS object
                 $aduser.Title = $user.title;
                 $aduser.DisplayName, $aduser.GivenName, $aduser.Surname = $user.name, $user.given_name, $user.surname;
                 $aduser.Company = $user.org_data.cost_centre.code;
@@ -77,16 +79,16 @@ try {
                 if ($user.parent__email -ne ($adusers | where distinguishedname -like $aduser.Manager).emailaddress) {
                     $aduser.Manager = ($adusers | where emailaddress -like $($user.parent__email)).DistinguishedName;
                 }
-                # make all of the undefined properties the string "N/A"
+                # ...make all of the undefined properties the string "N/A"
                 foreach ($prop in $aduser.ModifiedProperties) { 
                     if ((-not $aduser.$prop) -and ($prop -notlike "manager")) {
                         $aduser.$prop = "N/A";
                     } 
                 }
-                # push changes back to AD
+                # ...push changes back to AD
                 try {
                     Set-ADUser -verbose -server $adserver -instance $aduser;
-                    # thumbnailPhoto isn't added as a property of $aduser for some dumb reason, so we have to push it seperately
+                    # (thumbnailPhoto isn't added as a property of $aduser for some dumb reason, so we have to push it seperately)
                     if ($user.photo_ad -and $user.photo_ad.startswith('http')) {
                         Set-ADUser -verbose -server $adserver $aduser -replace @{thumbnailPhoto=$(Invoke-WebRequest $user.photo_ad -WebSession $oimsession).content};
                     }
@@ -100,35 +102,39 @@ try {
                     Log $($except | convertto-json);
                 }
             }
-            # If the AD object was modified after the OIM CMS object, sync to that system
+            # If the AD object was modified after the OIM CMS object, sync back to the CMS...
             if ($aduser.Modified -gt $(Get-Date $user.ad_data.Modified)) {
-                # find the mailbox object
+                # ...find the mailbox object
                 $mb = $mailboxes | where userprincipalname -like $user.email;
-                # glom the mailbox object onto the AD object
+                # ...glom the mailbox object onto the AD object
                 $simpleuser = $aduser | select ObjectGUID, @{name="mailbox";expression={$mb}}, @{name="Modified";expression={Get-Date $_.Modified -Format s}}, info, DistinguishedName, Name, Title, SamAccountName, GivenName, Surname, EmailAddress, Enabled, AccountExpirationDate, pwdLastSet;
                 $simpleuser | Add-Member -type NoteProperty -name PasswordMaxAgeDays -value $DefaultmaxPasswordAgeDays;
                 if ($aduser.AccountExpirationDate) { 
                     $simpleuser.AccountExpirationDate = Get-Date $aduser.AccountExpirationDate -Format s;
                 }
-                # convert the whole lot to JSON and push to OIM CMS
+                # ...convert the whole lot to JSON and push to OIM CMS via the REST API.
                 $userjson = [System.Text.Encoding]::UTF8.GetBytes($($simpleuser | ConvertTo-Json));
                 try {
+                    # Invoke the API.
                     $ad_data = (Invoke-RestMethod $user_api -Body $userjson -Method Post -ContentType "application/json" -WebSession $oimsession).ad_data;
                 } catch [System.Exception] {
+                    # Log any failures to sync AD data into the OIM CMS, for reference.
                     Log $("ERROR: update OIM CMS failed on {0}" -f $user.email);
                     Log $($simpleuser | ConvertTo-Json);
                 }
             }
         } 
 
-        # If the user is enabled, update AD data field in OIM CMS
-        if (($aduser.enabled -eq $false)) {
+        # If the user is enabled in AD, update AD data field in OIM CMS.
+        if ($aduser.enabled -eq $false) {
             if (-not $user.ad_deleted) {
                 $simpleuser = $aduser | select ObjectGUID, @{name="mailbox";expression={$mb}}, @{name="Modified";expression={Get-Date $_.Modified -Format s}}, info, DistinguishedName, Name, Title, SamAccountName, GivenName, Surname, EmailAddress, Enabled, AccountExpirationDate, pwdLastSet;
                 $userjson = [System.Text.Encoding]::UTF8.GetBytes($($simpleuser | ConvertTo-Json));
                 try {
+                    # Invoke the API.
                     $ad_data = (Invoke-RestMethod $user_api -Body $userjson -Method Post -ContentType "application/json" -Verbose -WebSession $oimsession).ad_data;
                 } catch [System.Exception] {
+                    # Log any failures to sync AD data into the OIM CMS, for reference.
                     Log $("ERROR: update OIM CMS failed on {0}" -f $user.email);
                     Log $($simpleuser | ConvertTo-Json);
                 }
@@ -136,19 +142,19 @@ try {
         }
     }
 
-    # we've done a whole pile of AD changes, so now's a good time to run AADSync to push them to O365
+    # We've done a whole pile of AD changes, so now's a good time to run AADSync to push them to O365:
     Log "Azure AD Connect Syncing with O365";
     Start-ADSyncSyncCycle -PolicyType Delta;
-    # this command is not blocking, and the new AAD Connect API is crap at polling for activity
+    # This command is not blocking and the new AAD Connect API is crap at polling for activity,
     # so let's just block for 60 seconds!
     Start-Sleep -s 60;
 
-    # finally, we want to do some operations on Office 365 accounts not handled by AADSync
-    # start by reading the full user list 
+    # Finally, we want to do some operations on Office 365 accounts not handled by AADSync.
+    # Start by reading the full user list.
     $msolusers = get-msoluser -all | select userprincipalname, lastdirsynctime, @{name="licenses";expression={[string]$_.licenses.accountskuid}}, signinname, immutableid, whencreated, displayname, firstname, lastname;
     $msolusers | convertto-json > 'C:\cron\msolusers.json';
 
-    # rig the UPN for each user account so that it matches the primary SMTP address.
+    # Rig the UPN for each user account so that it matches the primary SMTP address.
     foreach ($aduser in $adusers | where {$_.emailaddress -ne $_.userprincipalname}) {
         $immutableid = [System.Convert]::ToBase64String($aduser.ObjectGuid.toByteArray());
         $msoluser = $msolusers | where immutableid -eq $immutableid;
@@ -160,24 +166,24 @@ try {
         }
     }
 
-    # for each Exchange Online mailbox that doesn't have it, add an archive mailbox
+    # For each Exchange Online mailbox that doesn't have it, add an archive mailbox:
     $mailboxes | where recipienttypedetails -like remoteusermailbox | where { $_.archivestatus -eq "None" } | foreach { 
         Enable-RemoteMailbox -Identity $_.userprincipalname -Archive;
     }
 
-    # for each Exchange Online mailbox where it doesn't match, set the PrimarySmtpAddress to match the UserPrincipalName
+    # For each Exchange Online mailbox where it doesn't match, set the PrimarySmtpAddress to match the UserPrincipalName:
     $mailboxes | where recipienttypedetails -like remoteusermailbox | where { $_.userprincipalname -ne $_.primarysmtpaddress } | foreach { 
         Set-RemoteMailbox $_.userprincipalname -PrimarySmtpAddress $_.userprincipalname -EmailAddressPolicyEnabled $false -Verbose;
     }
     
-    # for each "In cloud" user in Azure AD which is licensed
+    # For each "In cloud" user in Azure AD which is licensed...
     ForEach ($msoluser in $msolusers | where lastdirsynctime -eq $null | where licenses) {
         $username = $msoluser.FirstName + $msoluser.LastName;
         if (!$username) {
             $username = $msoluser.UserPrincipalName.Split("@", 2)[0]
         }
         $username = $username.Substring(0,[System.Math]::Min(15, $username.Length));
-        # link existing users
+        # ...link existing users
         $upn = $msoluser.UserPrincipalName;
         $existing = Get-ADUser -Filter { UserPrincipalName -like $upn };
         if ($existing) {
@@ -185,20 +191,20 @@ try {
             Set-MsolUser -UserPrincipalName $upn -ImmutableId $immutableid;
             continue;
         }
-        # Create new user
+        # ...create new user
         Log $("About to create O365 user: New-ADUser $username -Verbose -Path `"OU=Users,OU=DPaW,dc=corporateict,dc=domain`" -Enabled $true -UserPrincipalName $($msoluser.UserPrincipalName) -EmailAddress $($msoluser.UserPrincipalName) -DisplayName $($msoluser.DisplayName) -GivenName $($msoluser.FirstName) -Surname $($msoluser.LastName) -PasswordNotRequired $true");
         New-ADUser $username -Verbose -Path "OU=Users,OU=DPaW,dc=corporateict,dc=domain" -Enabled $true -UserPrincipalName $msoluser.UserPrincipalName -EmailAddress $msoluser.UserPrincipalName -DisplayName $msoluser.DisplayName -GivenName $msoluser.FirstName -Surname $msoluser.LastName -PasswordNotRequired $true;
-        # wait for changes to propagate
+        # ...wait for changes to propagate
         sleep 10;
-        # assume RemoteRoutingAddress name is the same base as the UPN
+        # ...assume RemoteRoutingAddress name is the same base as the UPN
         $rra = $msoluser.UserPrincipalName.Split("@", 2)[0]+"@dpaw.mail.onmicrosoft.com";
         Set-ADUser -Identity $username -Add @{'proxyAddresses'='SMTP:'+$msoluser.UserPrincipalName};
         Set-ADUser -Identity $username -Add @{'proxyAddresses'='smtp:'+$rra};
-        # add remotemailbox object
+        # ...add remotemailbox object
         Enable-RemoteMailbox -Identity $msoluser.UserPrincipalName -PrimarySmtpAddress $msoluser.UserPrincipalName -RemoteRoutingAddress $rra;
     }
 
-    # quick loop to fix RemteRoutingAddress; previously some RemoteMailbox objects were provisioned manually with the wrong one
+    # Quick loop to fix RemteRoutingAddress; previously some RemoteMailbox objects were provisioned manually with the wrong one.
     ForEach ($mb in Get-RemoteMailbox -ResultSize Unlimited | Where {-not ($_.RemoteRoutingAddress -like "*@dpaw.mail.onmicrosoft.com" )}) {
         $remote = $mb.EmailAddresses.SmtpAddress | Where {$_ -like "*@dpaw.mail.onmicrosoft.com"} | Select -First 1;
         If ($remote) {
@@ -213,5 +219,5 @@ try {
     Log $($except | convertto-json);
 }
 
-# cleanup
+# Final clean up.
 Get-PSSession | Remove-PSSession;
