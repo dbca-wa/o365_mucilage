d2519899afd5d69060db9400da42ecdd4c048675
Brendan Cale <brendan.cale@dpaw.wa.gov.au>
2017-05-17 15:45:05 +0800

    Stopped get-aduser from trying to filter on null smtp addresses

--- group_wrangler.ps1
+++ group_wrangler.ps1
@@ -30,7 +30,7 @@ try {
         }
         $lmembers = Get-ADGroupMember $group;
         if (-not $lmembers) { $lmembers = @() };
-        $members = $ugrp | Invoke-command -session $session -Command { get-unifiedgrouplinks -linktype Members} | foreach { Get-ADUser -Filter "EmailAddress -like `"$($_.PrimarySMTPAddress)`"" }
+        $members = $ugrp | Invoke-command -session $session -Command { get-unifiedgrouplinks -linktype Members} | foreach { if($_.PrimarySMTPAddress){ Get-ADUser -Filter "EmailAddress -like `"$($_.PrimarySMTPAddress)`"" }}
         if (-not $members) { $members = @() };
         # Update memberships
         $diff = Compare-Object $lmembers $members;
